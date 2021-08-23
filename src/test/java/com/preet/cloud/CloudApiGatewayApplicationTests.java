5cb3498f5f22cebb535cb56571ad0c970e5bbfc2
gurpreet <get2gpreet@gmail.com>
2021-08-23 23:04:08 +0530

    Initial configuration

---
+++ src/test/java/com/preet/cloud/CloudApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.preet.cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
